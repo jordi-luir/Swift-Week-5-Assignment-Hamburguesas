f81090a6d769f688ba20315770b2a5880946b8f5
jordi-luir <jordi.luir@gmail.com>
2016-03-26 02:07:17 +0100

    Mejora en el posicionamiento de los elementos en el View

--- Hamburguesas/ViewController.swift
+++ Hamburguesas/ViewController.swift
@@ -16,6 +16,12 @@ class ViewController: UIViewController {
     // Una instancia de la clase ColeccionDeHamburguesas
     let coleccionDeHamburguesas = ColeccionDeHamburguesas()
 
+    // Referencia a la etiqueta de Nombre de país
+    @IBOutlet weak var nombreDePais: UILabel!
+    
+    // Referencia a la etiqueta de Nombre de hamburguesa
+    @IBOutlet weak var nombreDeHamburguesa: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,7 +34,8 @@ class ViewController: UIViewController {
 
     // Un @IBAction para implementar cambiar de país y de hamburguesa
     @IBAction func dameUnaHamburguesa() {
-        print("Cambiar de país y de hamburguesa")
+        nombreDePais.text = coleccionDePaises.obtenPais()
+        nombreDeHamburguesa.text = coleccionDeHamburguesas.obtenHamburguesa()
     }
 
 }
